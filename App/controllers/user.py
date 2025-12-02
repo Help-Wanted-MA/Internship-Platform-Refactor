e2ece6dd5703a6510e8df0b0a80fe07f64735c40
Nishant Rogers <157635543+NishantRogers@users.noreply.github.com>
2025-12-01 20:41:40 -0400

    Update user.py

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -1,30 +1,6 @@
 from App.models import User, Student, Employer, Staff
 from App.database import db
 
-def create_user(username, password, user_type):
-    try:
-        newuser = User(username=username, password=password, role=user_type)
-        db.session.add(newuser)
-        db.session.flush() 
-        
-        if user_type == "student":
-            student = Student(username=username, user_id=newuser.id)
-            db.session.add(student)
-        elif user_type == "employer":
-            employer = Employer(username=username, user_id=newuser.id)
-            db.session.add(employer)
-        elif user_type == "staff":
-            staff = Staff(username=username, user_id=newuser.id)
-            db.session.add(staff)
-        else:
-            return False
-        
-        db.session.commit()
-        return True
-    except Exception as e:
-        db.session.rollback()
-        return False
-
 
 def get_user_by_username(username):
     result = db.session.execute(db.select(User).filter_by(username=username))
